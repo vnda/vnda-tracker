bd5d86fbfb89c789d5a72251ea6a424859168472
Francisco DC <fdc@razaoinfo.com.br>
2017-04-07 11:51:22 -0300

    generate default token

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,11 @@
 class Shop < ApplicationRecord
   has_many :trackings, dependent: :destroy
+
+  before_validation :default_token
+
+  protected
+
+  def default_token
+    self.token = SecureRandom.hex(16) unless self.token.presence
+  end
 end
