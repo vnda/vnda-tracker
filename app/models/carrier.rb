045b15d57e1f9db0c37b524dbbe0553364448609
Francisco DC <fdc@razaoinfo.com.br>
2017-05-04 10:15:30 -0300

    tnt url

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -9,11 +9,13 @@ class Carrier
 
   def self.discover(code)
     return "correios" if code =~ /[A-Z]{2}[0-9]{9}[A-Z]{2}/
+    return "tnt" if code =~ /^.{12}$/
     "unknown"
   end
 
   def self.url(carrier, code)
     return "https://track.aftership.com/#{code}" if carrier == "correios"
+    return "http://app.tntbrasil.com.br/radar/public/localizacaoSimplificadaDetail/#{code}" if carrier == "tnt"
     ""
   end
 
@@ -22,6 +24,8 @@ class Carrier
   def service
     @service ||= if @carrier == "correios"
       Correios.new
+    elsif @carrier == "tnt"
+      Tnt.new
     else
       raise "Unsupported Carrier"
     end
