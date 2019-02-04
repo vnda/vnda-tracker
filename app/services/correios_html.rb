d79d494cb2f4510394d76788a6a55c9af56dd6e1
Unknown <fdc@razaoinfo.com.br>
2019-02-04 13:28:17 -0200

    fix CorreiosHtml status parser

--- app/services/correios_html.rb
+++ app/services/correios_html.rb
@@ -4,6 +4,22 @@ class CorreiosHtml
   URL = 'https://www2.correios.com.br/sistemas/rastreamento/resultado_semcont' \
     'ent.cfm'
 
+  STATUS_MAPPING = {
+    'Objeto postado' => 'in_transit',
+    'Postado depois do hor.*rio limite da ag.*ncia' => 'in_transit',
+    'Objeto encaminhado' => 'in_transit',
+    'Saiu para Entrega' => 'out_of_delivery',
+    'A entrega n.*o pode ser efetuada' => 'out_of_delivery',
+    'A entrega ocorrer.* no prox dia .til' => 'out_of_delivery',
+    'Logradouro com numera.*o irregular' => 'out_of_delivery',
+    'Coleta ou entrega de objeto n.*o efetuada' => 'out_of_delivery',
+    'Tentativa de entrega não efetuada' => 'out_of_delivery',
+    'Sa.*da para entrega cancelada' => 'out_of_delivery',
+    'Objeto saiu para entrega ao destinat.*rio' => 'out_of_delivery',
+    'Objeto entregue' => 'delivered',
+    'Objeto devolvido' => 'expired'
+  }.freeze
+
   def status(tracking_code)
     begin
       response = request(tracking_code)
@@ -14,6 +30,14 @@ class CorreiosHtml
     event = parse(response.body)
     return { date: nil, status: 'pending', message: nil } unless event
     event
+  end
+
+  def parse_status(text)
+    STATUS_MAPPING.each do |regex, status|
+      return status if Regexp.new(regex).match?(text)
+    end
+
+    'exception'
   end
 
   private
@@ -43,15 +67,5 @@ class CorreiosHtml
     array = text.split("\r\n").select(&:present?).map(&:strip)
     date, time, _city = array
     "#{date} #{time}".to_datetime
-  end
-
-  def parse_status(text)
-    {
-      'Objeto postado' => 'in_transit',
-      'Objeto encaminhado' => 'in_transit',
-      'Objeto saiu para entrega ao destinatário' => 'out_of_delivery',
-      'Objeto entregue ao destinatário' => 'delivered',
-      'Objeto devolvido ao remetente' => 'expired'
-    }.fetch(text, 'exception')
   end
 end
