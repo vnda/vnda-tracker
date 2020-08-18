ce2c1dd05f33cbd140704fc4472ebebe3bda9d18
Francisco <franciscojatem@gmail.com>
2020-08-18 12:19:08 -0300

    store all responses from Correios

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_153810) do
+ActiveRecord::Schema.define(version: 2020_08_18_150116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "correios_histories", force: :cascade do |t|
+    t.string "code", null: false
+    t.text "response_body"
+    t.integer "response_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", id: :serial, force: :cascade do |t|
     t.string "name"
